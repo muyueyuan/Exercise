6c16574af8834d66e53742020e243c7538bc6f09
muyueyuan <105255969+muyueyuan@users.noreply.github.com>
2023-02-16 07:25:32 +0800

    更新 yiyan1.js

--- yiyan1.js
+++ yiyan1.js
@@ -8,7 +8,7 @@ hostname = app.yiyan.art
 var body = $response.body; // 声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
 
-obj.u.viptype = "4";
+obj.user.viptype = "4";
 
 body = JSON.stringify(obj); // 重新打包回json字符串
 $done(body); // 结束修改
