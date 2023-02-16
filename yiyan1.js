d3f194b877cf8209fe034d6f5f249f4862d70ba9
muyueyuan <105255969+muyueyuan@users.noreply.github.com>
2023-02-16 12:32:55 +0800

    更新 ali.js

--- yiyan1.js
+++ yiyan1.js
@@ -1,34 +1,19 @@
 [rewrite_local]
-#一言
-^https:\/\/app\.yiyan\.art\/yiyan\/getuserinfoandbooklist?YanYan=2&uid=3936845&v=4\.15 url script-response-body https://raw.githubusercontent.com/muyueyuan/Exercise/main/yiyan1.js
+#阿里
+^https:\/\/api\.aliyundrive\.com url script-response-body ali.js
 
 [MITM]
-hostname = app.yiyan.art
+hostname = api.aliyundrive.com
 
-var body = $response.body; // 声明一个变量body并以响应消息体赋值
-var objc = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
 
-objc = {
-  "user": {
-    "uid": "3936845",
-    "smallavatar": "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLWMx3KMBra5dibKiciaxexLJce8icYQH3K3ykhGicSF0tCYkVc01iaK7Lq0O8wjJDePjafgb5B4icFLAYWQ/132",
-    "actype": "2",
-    "gender": "F",
-    "viptype": "4",
-    "fanscnt": "0",
-    "largeavatar": "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLWMx3KMBra5dibKiciaxexLJce8icYQH3K3ykhGicSF0tCYkVc01iaK7Lq0O8wjJDePjafgb5B4icFLAYWQ/132",
-    "device": "0",
-    "followcnt": "1",
-    "username": "佳思的唯一爸爸"
-  },
-  "booklist": [{
-    "datetime": "2023-02-16 06:26:50.0",
-    "bookname": "默认文集",
-    "ava": "1",
-    "bookid": "5665728"
-  }]
-};
+const vip = “/business/v1.0/users/vip/info”
 
-body = JSON.stringify(obj); // 重新打包回json字符串
-$done(body); // 结束修改
+if (url.indexOf(vip) != -1) {
+      obj.data.expire = 253402255586;
+      body = JSON.stringify(obj);
+}
 
+$done({body});
