80aacd244086756fe45c99f5dc900aa1de30f5c4
muyueyuan <105255969+muyueyuan@users.noreply.github.com>
2023-02-16 08:42:21 +0800

    更新 yiyan1.js

--- yiyan1.js
+++ yiyan1.js
@@ -1,6 +1,6 @@
 [rewrite_local]
 #一言
-^https:\/\/app\.yiyan\.art\/yiyan\/getuserinfoandbooklist.+$ url script-response-body yiyan1.js
+^https:\/\/app\.yiyan\.art\/yiyan\/getuserinfoandbooklist url script-response-body yiyan1.js
 
 [MITM]
 hostname = app.yiyan.art
@@ -8,7 +8,26 @@ hostname = app.yiyan.art
 var body = $response.body; // 声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
 
-obj.user.viptype = "4";
+obj = {
+  "user": {
+    "uid": "3936845",
+    "smallavatar": "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLWMx3KMBra5dibKiciaxexLJce8icYQH3K3ykhGicSF0tCYkVc01iaK7Lq0O8wjJDePjafgb5B4icFLAYWQ/132",
+    "actype": "2",
+    "gender": "F",
+    "viptype": "4",
+    "fanscnt": "0",
+    "largeavatar": "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLWMx3KMBra5dibKiciaxexLJce8icYQH3K3ykhGicSF0tCYkVc01iaK7Lq0O8wjJDePjafgb5B4icFLAYWQ/132",
+    "device": "0",
+    "followcnt": "1",
+    "username": "佳思的唯一爸爸"
+  },
+  "booklist": [{
+    "datetime": "2023-02-16 06:26:50.0",
+    "bookname": "默认文集",
+    "ava": "1",
+    "bookid": "5665728"
+  }]
+};
 
 body = JSON.stringify(obj); // 重新打包回json字符串
 $done(body); // 结束修改
